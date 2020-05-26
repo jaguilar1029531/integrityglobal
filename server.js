a2ba2c8ebebe4d4df2264996d2f21e20335dee12
Jorge Aguilar <aguila.techserv@gmail.com>
2020-05-26 18:58:49 -0500

    dfjksfsdlkjfdlkj

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", );
+mongoose.connect(process.env.MONGODB_URI || "mongodb://jaguilar1029531:gutt3rch1ld@ds041546.mlab.com:41546/heroku_fvcl1mpg", );
 
 // Start the API server
 app.listen(PORT, function() {
