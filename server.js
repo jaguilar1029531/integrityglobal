1a82ebd904f257be3a406722eab681cd01a40104
Jorge Aguilar <aguila.techserv@gmail.com>
2020-05-26 18:09:58 -0500

    fdsfdf

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://jaguilar1029531:gutt3rch1ld@cluster0-oovdw.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
