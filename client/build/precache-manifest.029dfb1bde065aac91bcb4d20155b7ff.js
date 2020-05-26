665094445bcee9c7af037184207612d25e74e276
Jorge Aguilar <aguila.techserv@gmail.com>
2020-05-26 17:32:20 -0500

    trying hreoku

---
+++ client/build/precache-manifest.029dfb1bde065aac91bcb4d20155b7ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6f1d39bdb70561ec83b",
+    "url": "/static/css/main.f40d9c60.chunk.css"
+  },
+  {
+    "revision": "f6f1d39bdb70561ec83b",
+    "url": "/static/js/main.dd088714.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "0fed812d266fefd4db012059a98f83f0",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+  
 const express = require("express");
 
 const mongoose = require("mongoose");
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/jorgeaguilarproject3");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {
