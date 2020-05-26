8bf91564a75d8208e4293583949cf99cc2a71c40
Jorge Aguilar <aguila.techserv@gmail.com>
2020-05-26 18:49:27 -0500

    fdjdfkldfslj

--- server.js
+++ server.js
@@ -18,10 +18,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", {
-  userNewUrlParser: true,
-  userUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", );
 
 // Start the API server
 app.listen(PORT, function() {
